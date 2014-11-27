1e44c48c675675dacb02282a76dbaeb0f9f3e604
Jakub Skokan <jakub.skokan@havefun.cz>
2014-11-27 19:19:01 +0100

    Key bindings: scrot, lock

--- xmonad.hs
+++ xmonad.hs
@@ -1,10 +1,25 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
 myTerminal = "konsole"
 
 main = do
-        xmonad $ defaultConfig {
-	         modMask = mod4Mask,
-	         terminal = myTerminal
-        }
+        xmonad $ defaultConfig
+                {
+                        manageHook = manageDocks <+> manageHook defaultConfig
+                        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+                        , modMask = mod4Mask
+                        , terminal = myTerminal
+                } `additionalKeys`
+                [
+                        ((mod4Mask .|. shiftMask, xK_z), spawn "lock")
+                        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+                        , ((0, xK_Print), spawn "scrot")
+                ]
 
+
+        
