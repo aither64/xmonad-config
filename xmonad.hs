aefc09a627b32bfa6adf4032f569417094c76171
Jakub Skokan <jakub.skokan@havefun.cz>
2014-11-27 20:30:28 +0100

    Show/hide xmobar

--- xmonad.hs
+++ xmonad.hs
@@ -16,25 +16,29 @@ myManageHook = composeAll
         , className =? "gmpc"                --> doShift "5"
         ]
 
-main = do
-        xmproc <- spawnPipe "xmobar /home/aither/.xmonad/xmobarrc"
-        xmonad $ defaultConfig
-                {
-                        workspaces = ["1","2","3","4","5","6","7","8","9"]
-                        , manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
-                        , layoutHook = avoidStruts  $  layoutHook defaultConfig
-                        , logHook = dynamicLogWithPP xmobarPP
-                                { ppOutput = hPutStrLn xmproc
-                                , ppTitle = xmobarColor "green" "" . shorten 50
-                                }
-                        , modMask = mod4Mask
-                        , terminal = myTerminal
-                } `additionalKeys`
-                [
-                        ((mod4Mask .|. shiftMask, xK_z), spawn "lock")
-                        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-                        , ((0, xK_Print), spawn "scrot")
-                ]
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+
+myBar = "xmobar /home/aither/.xmonad/xmobarrc"
+
+myPP = defaultPP
+        { ppTitle = xmobarColor "#02a2ff" "" . shorten 50
+        }
+
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+myConfig = defaultConfig
+        {
+                workspaces = ["1","2","3","4","5","6","7","8","9"]
+                , manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
+                , layoutHook = avoidStruts  $  layoutHook defaultConfig
+                , modMask = mod4Mask
+                , terminal = myTerminal
+        } `additionalKeys`
+        [
+                ((mod4Mask .|. shiftMask, xK_z), spawn "lock")
+                , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+                , ((0, xK_Print), spawn "scrot")
+        ]
 
 
         
